e7a146fe63d7ea857559f5e14207dc5d8b7e4f5c
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-08-02 11:04:28 -0400

    iniciando a parte de usuários

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
+const PORT = 8081;
+    // express
 const express = require('express');
 const app = express();
-const PORT = 8081;
+    // express-handlebars
+const handlebars = require('express-handlebars');
+    // path - bootstrap
+const path = require('path');
+    // mongoose
+const mongoose = require('mongoose');
+
+
+/// configs
+    // handlebars
+    app.engine('handlebars', handlebars.engine({
+        defaultLayout: 'main', 
+            runtimeOptions: {
+                allowProtoPropertiesByDefault: true,
+                allowProtoMethodsByDefault: true,
+            } 
+    }));
+    app.set('view engine', 'handlebars');
+
+    // bootstrap - public
+    app.use(express.static(path.join(__dirname + "/public")));
+
+    // mongoose
+    mongoose.connect('mongodb://localhost/ttapp')
+    .then(() => {
+        console.log('mongodb conectado!');
+    }).catch((err) => {
+        console.log('erro ao conectar com o banco!')
+    });
 
 
 /// Rotas
 app.get('/', (req, res) => {
-    res.send('TUDO CERTO ATÉ AQUI');
+    res.render('public/index');
 });
 
+
+
+/// Outros
 app.listen(PORT, () => {
     console.log('servidor está rodando, na porta: ' + PORT);
 });
